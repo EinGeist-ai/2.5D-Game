638afc83451f4dcfaec9642d420b802afeb350b7
Henrik <henrikmartens82@gmail.com>
2025-07-08 18:49:08 +0200

    Detection WOwie

--- Assets/Scripts/Enemys/EnemyMovement 2.cs
+++ Assets/Scripts/Enemys/EnemyMovement 2.cs
@@ -7,13 +7,32 @@ public class EnemyMovement2 : MonoBehaviour
 {
     public GameObject player;
     public NavMeshAgent enemy;
-
+    public GameObject enemyG;
+    public bool isAgitaed = false;
+    RaycastHit hit;
+    public float detectionRange = 10f;
+    
     // Update is called once per frame
     void Update()
     {
-       Vector3 playerPos = player.transform.position;
+        Vector3 direction = player.transform.position - enemyG.transform.position;
+    float distance = Vector3.Distance(player.transform.position, enemyG.transform.position);
+        if (distance <= detectionRange)
+        {
+            Debug.DrawRay(enemyG.transform.position, direction, Color.red);
+            if (Physics.Raycast(enemyG.transform.position,  direction, out hit, detectionRange))
+            {
+                isAgitaed = true;
+                Debug.Log("Enemy is agitated and can see the player.");
+                Debug.DrawRay(enemyG.transform.position, player.transform.position, Color.green);
+            }
+        }
+        if (isAgitaed == true)
+        {
+            Vector3 playerPos = player.transform.position;
 
-        enemy.SetDestination(playerPos);
+            enemy.SetDestination(playerPos);
+        }
     }
 
    public IEnumerator EnemySlow(float slowDuration, float slowFactor)
